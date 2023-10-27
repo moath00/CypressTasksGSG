4a88b8f3a2ed67177ea0327271f0cc523ed935ef
moath00 <moath.n.h20@gmail.com>
2023-10-27 21:48:04 +0300

    Final task

--- cypress/e2e/e2eObjects/login.ts
+++ cypress/e2e/e2eObjects/login.ts
@@ -2,7 +2,9 @@ class LogIn {
     elements = {
         userNameField: () => cy.get('[placeholder="Username"]'),
         passwordField: () => cy.get('[placeholder="Password"]'),
-        loginBtn: () => cy.get('[type="submit"]')
+        loginBtn: () => cy.get('[type="submit"]'),
+        logoutList: () => cy.get('.oxd-userdropdown-tab'),
+        logoutBtn: () => cy.get('.oxd-topbar-header-userarea > ul > li > ul > li:nth-child(4) > a')
     };
 
     passedLogin(username: string, password: string): void {
@@ -10,6 +12,11 @@ class LogIn {
         this.elements.passwordField().type(password);
         this.elements.loginBtn().click();
     };
+
+    logOutLoggedUser() {
+        this.elements.logoutList().click();
+        this.elements.logoutBtn().click();
+    }
 }
 
 export default LogIn;
